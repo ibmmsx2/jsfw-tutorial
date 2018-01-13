bc73515044c3678da5740adf78cb61deee7d0e13
tida <ibmmsx2@gmail.com>
2018-01-14 00:53:58 +0900

    nodejs tutorial

--- express/server.js
+++ express/server.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
-var router = require('./router/main')(app);
+var bodyParser = require('body-parser');
+var session = require('express-session');
+var fs = require('fs');
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
@@ -11,3 +13,15 @@ var server = app.listen(3000, function(){
 })
 
 app.use(express.static('public'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended:true}
+));
+app.use(session({
+    secret: '@#@$MYSIGN#@$#$',
+    resave: false,
+    saveUninitialized: true
+}));
+
+var router = require('./router/main')(app, fs);
