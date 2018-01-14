e2204da9012776cdbde94d03ba08c4d4bd69e9e6
tida <ibmmsx2@gmail.com>
2018-01-14 16:24:34 +0900

    react basic

---
+++ react-basic/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+
+const rootElement = document.getElementById('root');
+ReactDOM.render(<App />, rootElement); 
+/*
+ReactDOM.render(<App headerTitle = "Welcome!"
+                     contentTitle = "Stranger, "
+                     contentBody = "Welcome to example app"/>, rootElement);
+*/
